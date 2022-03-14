b5b7c9b547f9315c09d6bce40596b55c548a171d
Francis Alonzo <44241055+supertoto29@users.noreply.github.com>
2022-03-14 09:25:09 -0400

    update pour le nouveau provider power

--- include/main.h
+++ include/main.h
@@ -4,14 +4,21 @@
 #include "mbed.h"
 #include "rtos.h"
 #include "pinDef.h"
-#include "powersupply_utility.h"
+#include "Utility/Utility.h"
 #include "INA226.h"
-#include "TC74A5.h"
+#include "TC74A5/TC74A5.h"
 #include "RS485/RS485.h"
 #include "RS485/RS485_definition.h"
 
 #define delay 300
 #define battery_delay 20
+
+#define nb_sensor 3
+#define nb_alimentation 4
+#define nb_samples 10
+
+// Power Supply Slave à définir ici (0 à 3)
+#define PSU_ID SLAVE_PSU0
 
 //###################################################
 //              I2C SLAVE DEFINITION
@@ -27,7 +34,7 @@
 #define CURRENTLSB  0.0004577636
 
 //###################################################
-//             PINOUT FONCTION DEFINITION
+//             PINOUT FONCTION DEFINITIONS
 //###################################################
 
 DigitalOut LedBatt1(LED_BATT1);
@@ -47,5 +54,33 @@ DigitalIn StatusMotor2(STATUSMOTOR_V2);
 DigitalIn Killswitch(KILLSWITCH);
 
 AnalogIn Battery_16V(INPUT_4S);
+
+//###################################################
+//             OBJECT DEFINITIONS
+//###################################################
+
+RS485 rs(PSU_ID);
+I2C i2c_bus(I2CSDA,I2CSCL);
+// INA226 sensorm1(&i2c_bus, adress16V1);
+// INA226 sensorm2(&i2c_bus, adress16V2);
+// INA226 sensor12v(&i2c_bus, adress12v);
+INA226 sensor[3] = {INA226(&i2c_bus, adress16V1), INA226(&i2c_bus, adress16V2), INA226(&i2c_bus, adress12v)};
+TC74A5 temperature(&i2c_bus, adressTemp);
+
+//###################################################
+//             THREAD DEFINITIONS
+//###################################################
+
+Thread sensorRead;
+Thread activationMotor;
+Thread readMotor;
+Thread threademergencystop;
+
+//###################################################
+//             GLOBAL VARIABLE DEFINITIONS
+//###################################################
+
+uint8_t enableMotor1;
+uint8_t enableMotor2;
 
 #endif
